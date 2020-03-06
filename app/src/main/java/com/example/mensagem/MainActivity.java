4b2778deecee7690a4d240953326c92017005572
DanielSiob <48354047+DanielSiob@users.noreply.github.com>
2020-03-06 12:03:59 +0000

    Criado a interface da atividade principal

--- app/src/main/java/com/example/mensagem/MainActivity.java
+++ app/src/main/java/com/example/mensagem/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mensagem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //todo: implementar interface
+    }
+
+    public void enviaMensagem(View view) {
+        //todo: enviar mensagem
     }
 }
