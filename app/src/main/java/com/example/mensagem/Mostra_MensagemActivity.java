9df4ae140a3b772b5dc2e30527e14692683b903a
DanielSiob <daniellpp99@gmail.com>
2020-03-11 20:48:33 +0000

    Enviar mensagem e mostrar no TextView2

--- app/src/main/java/com/example/mensagem/Mostra_MensagemActivity.java
+++ app/src/main/java/com/example/mensagem/Mostra_MensagemActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Mostra_MensagemActivity extends AppCompatActivity {
@@ -15,7 +16,9 @@ public class Mostra_MensagemActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String mensagem = intent.getStringExtra("MENSAGEM");
-
         Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
+
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setText(mensagem);
     }
 }
